4fbc43b3d8a9f9bc588cf22e328f81c8438ee432
sebastian <sebas61018@gmail.com>
2023-10-04 16:14:13 -0500

    creacion del modelo de categoria

--- src/main/java/com/sistema/inventario/model/CategoryModel.java
+++ src/main/java/com/sistema/inventario/model/CategoryModel.java
@@ -1,2 +1,18 @@
-package com.sistema.inventario.model;public class CategoryModel {
+package com.sistema.inventario.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "category")
+@Data
+public class CategoryModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nameCategory;
+    private String description;
+    private String status;
+    private String displayOrder;
 }
+
