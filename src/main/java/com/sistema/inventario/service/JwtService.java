f4063398b369ee413f0bd5a4fc7460f69e06011c
manuel carrascal <tricdiart@gmail.com>
2023-11-17 15:11:20 -0500

    Corrections packages

--- src/main/java/com/sistema/inventario/service/JwtService.java
+++ src/main/java/com/sistema/inventario/service/JwtService.java
@@ -25,7 +25,7 @@ public class JwtService {
         return Jwts.builder().
                 setClaims(extraClaims).
                 setSubject(user.getUsername()).
-                setIssuedAt(new Date(System.currentTimeMillis())). //Fecha de creacion
+                setIssuedAt(new Date(System.currentTimeMillis())).
                 setExpiration(new Date(System.currentTimeMillis() + 1000 *60*24)).
                 signWith(getKey(), SignatureAlgorithm.HS256).
                 compact();
