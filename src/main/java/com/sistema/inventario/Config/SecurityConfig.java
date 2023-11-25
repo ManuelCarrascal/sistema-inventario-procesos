d492b77723e5eb3f5f1104c7d0a725e376e0084b
Manuel Carrascal <54221416+ManuelCarrascal@users.noreply.github.com>
2023-11-25 13:11:01 -0500

    Update SecurityConfig.java

--- src/main/java/com/sistema/inventario/Config/SecurityConfig.java
+++ src/main/java/com/sistema/inventario/Config/SecurityConfig.java
@@ -1,14 +1,22 @@
-package com.sistema.inventario.config;
+package com.sistema.inventario.Config;
 
-import com.sistema.inventario.jwt.JwtAuthentificationFilter;
+import com.sistema.inventario.exception.NotFoundException;
+import com.sistema.inventario.filter.JwtAuthentificationFilter;
+import com.sistema.inventario.repository.UserRepository;
+import com.sistema.inventario.util.Constants;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -18,21 +26,39 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthentificationFilter jwtAuthentificationFilter;
-    private final AuthenticationProvider authenticationProvider;
-
+    private final UserRepository userRepository;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http.csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authRequest ->
-                        authRequest
-                                .requestMatchers("/auth/**").permitAll()
-                                .anyRequest().permitAll()
-                ).
-                sessionManagement(sessonManager ->
-                        sessonManager.
-                        sessionCreationPolicy(SessionCreationPolicy.STATELESS)).
-                authenticationProvider(authenticationProvider).
-                addFilterBefore(jwtAuthentificationFilter, UsernamePasswordAuthenticationFilter.class)
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers("/auth/**").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .exceptionHandling(httpSecurityExceptionHandlingConfigurer ->
+                        httpSecurityExceptionHandlingConfigurer.authenticationEntryPoint(
+                                (request, response, authException) -> {
+                                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+                                }
+                        )
+                )
+                .addFilterBefore(jwtAuthentificationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+        return email -> userRepository.findByEmail(email).orElseThrow(() ->
+                new NotFoundException(Constants.CREDENTIAL_INVALID.getMessage()));
     }
 }
